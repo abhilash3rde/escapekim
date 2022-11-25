b1a35a002b7e6cc29d9e03760e869b184b44d33e
sanjay kumar <sanjay3rde@gmail.com>
2022-11-25 16:33:17 +0530

    fixed UI desktop version

--- components/Desktopview/ActFirstDesktop.js
+++ components/Desktopview/ActFirstDesktop.js
@@ -20,18 +20,18 @@ export const ActFirstDesktop = () => {
         </div>
 
 
-        <div className='grid grid-cols-1 '>
+        <div className='flex justify-center w-full'>
           <img src={'/DesktopImages/Billboard_fullSize.png'} alt="" className='' />
         </div>
 
         <div className=' relative flex  justify-between w-[100%] gap-1'>
-          <div className='grid grid-cols-2 w-[90%]'>
+          <div className='grid grid-cols-2 w-[100%]'>
             <img src={'/DesktopImages/Cloud01.png'} alt="" className='' />
             <div className='flex justify-start items-center '>
-              <img src={'DesktopImages/silhouette-dude@2x.png'} alt="" className='h-[62px] w-[66px] z-50' />
+              <img src={'DesktopImages/silhouette-dude@2x.png'} alt="" className='h-[62px] w-[66px] z-50 absolute left-[47%]' />
             </div>
           </div>
-          <div className='absolute top-[-270px] grid grid-cols-2'>
+          <div className='absolute top-[-200px] grid grid-cols-2 flex justify-end'>
             <div className='col-start-2'>
               <img src={'/DesktopImages/Cloud02.png'} alt="" className=' ' />
             </div>
@@ -44,21 +44,21 @@ export const ActFirstDesktop = () => {
 
             <div className=' relative flex  grid grid-cols-1 '>
               <img src={'/DesktopImages/cs01.png'} alt="" className='w-full h-[362px] z-50' />
-             
-                <div className=' flex justify-center '>
-                  <div className='w-[80%] grid  grid-cols-2 absolute  top-[-20px]'>
-                    <div className=' px-2 py-2 z-50 border-[#070707] w-[275px] h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
-                      <span className='text-[14px] uppercase font-bold '>He’s not going to make it…</span>
-                    </div>
+
+              <div className=' flex justify-center '>
+                <div className='w-[80%] grid  grid-cols-2 absolute  top-[-20px]'>
+                  <div className=' px-2 py-2 z-50 border-[#070707] w-[275px] h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
+                    <span className='text-[14px] uppercase font-bold '>He’s not going to make it…</span>
                   </div>
+                </div>
 
-                </div >
+              </div >
 
-                <div className=' flex justify-center '>
+              <div className=' flex justify-center '>
                 <div className='w-[95%] grid  grid-cols-2 absolute  top-[28px]'>
                   <div className=' px-2 py-2 z-50 border-[#070707] w-[34.562rem]  h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
                     <span className='text-[14px] uppercase font-bold '>
-                    You know that is not for you to decide… that is up to him.
+                      You know that is not for you to decide… that is up to him.
                     </span>
                   </div>
                 </div>
@@ -67,7 +67,7 @@ export const ActFirstDesktop = () => {
                 <div className='w-[95%] grid  grid-cols-2 absolute  top-[76px]'>
                   <div className=' px-2 py-2 z-50 border-[#070707] w-[10.688rem]  h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
                     <span className='text-[14px] uppercase font-bold '>
-                    There is hope…
+                      There is hope…
                     </span>
                   </div>
                 </div>
@@ -76,8 +76,8 @@ export const ActFirstDesktop = () => {
 
 
             <div className=' flex justify-center '>
-              <div className='w-[80%] grid  grid-cols-2 mt-2'>
-                <div className='px-2 py-2 z-50 border-[#070707] w-[37rem]  items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
+              <div className='w-[80%] grid  grid-cols-3 mt-2'>
+                <div className='col-span-2 px-2 py-2 z-50 border-[#070707]  items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
                   <span className='text-[14px] uppercase font-bold '>He’s fat and soft. a trigger happy child is what he is. he’s not worked a day in his life. how do you suppose he’s to survive this game we’re set for him…..</span>
                 </div>
               </div>
@@ -87,16 +87,16 @@ export const ActFirstDesktop = () => {
             <div className=' relative flex justify-center mt-6'>
               <div className='w-[90%] grid  grid-cols-1'>
                 <img src={'/DesktopImages/cs-jump02.png'} alt="" className='w-full h-[268px] ' />
-               
-                 <div className=' flex justify-center '>
-                <div className='w-[95%] grid  grid-cols-2 absolute  top-[3px]'>
-                  <div className=' px-2 py-2 z-50 border-[#070707] w-[18.375rem]  h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
-                    <span className='text-[14px] uppercase font-bold '>
-                    YOU KNOW why he’s like this?
-                    </span>
+
+                <div className=' flex justify-center '>
+                  <div className='w-[95%] grid  grid-cols-2 absolute  top-[3px]'>
+                    <div className=' px-2 py-2 z-50 border-[#070707] w-[18.375rem]  h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
+                      <span className='text-[14px] uppercase font-bold '>
+                        YOU KNOW why he’s like this?
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
 
                 <div className='my-2  flex justify-center '>
                   <div className='w-[80%] grid  grid-cols-2 absolute  top-[50px]'>
@@ -107,14 +107,14 @@ export const ActFirstDesktop = () => {
 
                 </div >
                 <div className=' flex justify-center '>
-                <div className='w-[95%] grid  grid-cols-2 absolute  top-[95px]'>
-                  <div className=' px-2 py-2 z-50 border-[#070707] w-[13.813rem]  h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
-                    <span className='text-[14px] uppercase font-bold '>
-                    He Won’t remember….
-                    </span>
+                  <div className='w-[95%] grid  grid-cols-2 absolute  top-[95px]'>
+                    <div className=' px-2 py-2 z-50 border-[#070707] w-[13.813rem]  h-[42px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
+                      <span className='text-[14px] uppercase font-bold '>
+                        He Won’t remember….
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
 
                 <div className=' absolute top-[155px] ' >
                   <div className=' grid  grid-cols-3'>
@@ -134,8 +134,8 @@ export const ActFirstDesktop = () => {
             </div>
 
 
-            <div className='my-2  flex justify-end '>
-              <div className='w-[90%] grid  grid-cols-3 mt-6  '>
+            <div className='mb-2 flex justify-end '>
+              <div className='w-[90%] grid  grid-cols-3 mt-4  '>
                 <div className=' col-start-3 px-2 py-2 h-[42px]  ml-[4.7rem] justify-end z-50 border-[#070707] right-0  items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
                   <span className='text-[14px] text-right uppercase font-bold '>… and there’s eights of us</span>
                 </div>
@@ -146,8 +146,8 @@ export const ActFirstDesktop = () => {
             <div className=' relative flex  grid grid-cols-1 '>
               <img src={'/DesktopImages/cs03last.png'} alt="" className='w-full h-[374px] z-50' />
               <div className='my-2  flex justify-center '>
-                <div className='w-[80%] grid  grid-cols-2 absolute  top-[0px]'>
-                  <div className=' px-2 py-2 z-50 border-[#070707] w-[36.81rem]  h-[87px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
+                <div className='w-[80%] grid  grid-cols-3 absolute  top-[0px]'>
+                  <div className=' col-span-2 px-2 py-2 z-50 border-[#070707]  h-[87px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
                     <span className='text-[14px] uppercase font-bold '>fuck him. he’s a parasite. he’s not playing for his evolution… he plays for his destruction. <br /><br /> chaos is all he knows.</span>
                   </div>
                 </div>
@@ -156,7 +156,7 @@ export const ActFirstDesktop = () => {
                 <div className='w-[95%] grid  grid-cols-2 absolute  bottom-[-15px]'>
                   <div className=' px-2 py-2 z-50 border-[#070707] w-[34.625rem]  h-[177px] items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
                     <span className='text-[14px] uppercase font-bold '>
-                    yes, he’s an asshole, but this is how its always been… <br /> sometimes we need asshole to teach us. <br /><br /> without shadow, light has nothing to reflect from. <br /> without shadow, none of us remember why we’re here. <br /><br /> This life is about a game… <br /> but only a few of us know how to unlock the cheat codes. <br /><br /> AND MAYBE HE WILL TOO…
+                      yes, he’s an asshole, but this is how its always been… <br /> sometimes we need asshole to teach us. <br /><br /> without shadow, light has nothing to reflect from. <br /> without shadow, none of us remember why we’re here. <br /><br /> This life is about a game… <br /> but only a few of us know how to unlock the cheat codes. <br /><br /> AND MAYBE HE WILL TOO…
                     </span>
                   </div>
                 </div>
@@ -165,33 +165,44 @@ export const ActFirstDesktop = () => {
           </div>
         </div>
 
-        <div className='  flex justify-center'>
-          <div className='w-[90%] grid  grid-cols-3 mt-[100px] mb-4 justify-center'>
-            <div className=' col-start-2  px-2 h-[42px] w-[21.5rem] py-2 z-50 border-[#070707] right-0  items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
+        <div className='grid  grid-cols-1'>
+          <div className=' mt-[100px] mb-4 flex justify-center'>
+            <div className='   px-2 h-[42px] w-[21.5rem] py-2 z-50 border-[#070707] right-0  items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
               <span className='text-[14px] text-center uppercase font-bold '>well, that is pricey why we built….</span>
             </div>
           </div>
         </div >
 
-        {/* <div className='grid  grid-cols-2' >
-          <img src={'/MobileImages/Cloud03.png'} alt="" className='w-full h-[158px] bg-transparent ' />
-        </div> */}
 
-        {/* <div className='grid  grid-cols-2' >
-          <img src={'/MobileImages/Cloud04.png'} alt="" className='w-full h-[158px] bg-transparent col-start-2' />
-          <div className='absolute flex-col  flex justify-center items-center w-[100%]'>
-            <img src={'/MobileImages/the_tesseract.png'} alt="" className=' h-[105px] w-[166px] bg-transparent col-start-2' />
-            <img src={'/MobileImages/silhouette-dude.png'} alt="" className=' h-[27px] z-10 w-[30px] bg-transparent' />
+        <div className=' relative grid grid-cols-3 w-full'>
+          <div className='' >
+            <img src={'/DesktopImages/Cloud03.png'} alt="" className='h-[37.5rem] w-[43.25rem] bg-transparent ' />
           </div>
-        </div> */}
+          <div className='flex-col  flex justify-center items-center  mt-[10.937rem]'>
+            <img src={'/DesktopImages/the_tesseract.png'} alt="" className=' h-[4.25rem] w-[15.375rem] bg-transparent col-start-2' />
+            <img src={'/DesktopImages/silhouette-dude01.png'} alt="" className=' h-[1.375rem] z-10 w-[1.5rem] bg-transparent' />
 
-        {/* <div className='grid grid-cols-1 relative flex justify-center' >
-          <img src={'/MobileImages/the-star.png'} alt="" className='w-full h-[602px] bg-transparent z-50 opacity-1 ' />
-          <div className='absolute top-[350px] left-0 right-0  flex justify-center items-center w-[100%]' >
-            <img src={'/MobileImages/Wormhole_03.png'} alt="" className='w-full h-[405px] opacity-1 bg-transparent ' />
+            <div className=' absolute top-[435px] grid grid-cols-1  flex justify-center' >
+              <img src={'/DesktopImages/the-star.png'} alt="" className='w-[44.812rem] h-[58.375rem] bg-transparent z-50 opacity-1 ' />
+
+            </div>
           </div>
+          <div className='flex justify-end' >
+            <img src={'/DesktopImages/Cloud04.png'} alt="" className='h-[34.625rem] w-[39.875rem] bg-transparent col-start-2' />
+          </div>
+        </div>
 
-        </div> */}
+        <div className='relative flex justify-center items-center w-[100%]' >
+          <img src={'/DesktopImages/Wormhole_03.png'} alt="" className='w-full h-[1080px] opacity-1 bg-transparent ' />
+          <div className='absolute bottom-[105px] grid w-[80%] grid-cols-3'>
+          <div className='col-start-3   flex justify-start'>
+            <div className='   px-2 h-[42px] w-[12rem] py-2 z-50 border-[#070707] right-0  items-center bg-[#FFE5A3] border-2 opacity-1  flex  flex-nowrap '>
+              <span className='text-[14px] text-center uppercase font-bold '>TO be continued….</span>
+            </div>
+          </div>
+        </div >
+        </div>
+
 
 
       </div>
